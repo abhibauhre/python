608cf65e8deb0420bc7ffba8082688ca02c9bbec
abhi bauhre <abhishekbohre77@gmail.com>
2025-08-21 17:22:05 +0530

    add some more files

---
+++ 11_functions/05_module.py
@@ -0,0 +1,9 @@
+# Two types of modules in Python: 
+# - Built in Modules 
+# - External Modules
+# List of all the built in Modules: https://docs.python.org/3/py-modindex.html
+import math 
+import mymodule
+
+print(math.sqrt(16))
+mymodule.hello()
