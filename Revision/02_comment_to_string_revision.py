a7faa743c0ba999001930d51d044fc71c41ddfd0
abhi bauhre <abhishekbohre77@gmail.com>
2026-09-09 23:25:12 +0530

    learn some questios

--- Revision/02_comment_to_string_revision.py
+++ Revision/02_comment_to_string_revision.py
@@ -70,3 +70,18 @@ for ch in s:
     if count[ch] == 1:
         print(ch)
         break
+
+numbers = [10, 5, 20, 8, 20, 15]
+
+largest = float('-inf')
+second_largest = float('-inf')
+
+for num in numbers:
+    if num > largest:
+        second_largest = largest
+        largest = num
+
+    elif num > second_largest and num != largest:
+        second_largest = num
+
+print(second_largest)    
