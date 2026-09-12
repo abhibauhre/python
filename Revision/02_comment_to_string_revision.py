502903bbe5b1ec5a68e2abe3348eb1fba7dc84ca
abhi bauhre <abhishekbohre77@gmail.com>
2026-09-12 23:50:25 +0530

    add

--- Revision/02_comment_to_string_revision.py
+++ Revision/02_comment_to_string_revision.py
@@ -114,3 +114,28 @@ for char in text:
     reverse = char + reverse
 
 print(reverse)
+def longest_consecutive(nums):
+    numbers = set(nums)
+    longest = 0
+
+    for num in numbers:
+
+        # Check if num is the beginning of a sequence
+        if num - 1 not in numbers:
+
+            current = num
+            length = 1
+
+            # Find consecutive numbers
+            while current + 1 in numbers:
+                current += 1
+                length += 1
+
+            longest = max(longest, length)
+
+    return longest
+
+
+nums = [100, 4, 200, 1, 3, 2]
+
+print(longest_consecutive(nums))
